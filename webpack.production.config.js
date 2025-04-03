8a9389b45679ddd147ecc7b0cf9f8adecca2d415
Dmytro Raiko <dmytro.r@giantleaplab.com>
2025-04-03 18:29:00 +0300

    initiRefactor React app structure and integrate Swagger UI
    Reorganized `index.html` for simplicity and updated `App.js` to include Swagger UI for API documentation. Added a production webpack configuration and included `swagger.json` to support API integration. These changes optimize the app for API interactions and streamlined builds.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,135 +1,73 @@
-const path = require('path')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const merge = require('lodash/merge')
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const webpack = require('webpack')
+module.exports = {
+  mode: 'production', // Set production mode for optimizations
 
-const envVars = merge(
-  require('dotenv').config({ path: '.env' }).parsed || {},
-  require('dotenv').config({ path: path.resolve(__dirname, '.env.prod') }).parsed || {},
-)
+  entry: './src/index.js', // Starting point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js', // Add content hash for cache-busting
+    publicPath: '/', // Serve files from root
+  },
 
-module.exports = (env) => {
-  const assetsVersionId = env.ASSETS_VERSION_ID || '12345'
-
-  return {
-    mode: 'production',
-    entry: {
-      back: ['core-js/stable', 'regenerator-runtime/runtime', './frontend/web/src/index'],
-      front: ['core-js/stable', 'regenerator-runtime/runtime', './frontend/web/src/front/index'],
-    },
-    optimization: {
-      minimize: true,
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            name: 'common',
-            chunks: 'initial',
-            minChunks: 2,
-          },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Transpile modern JavaScript
         },
       },
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-          },
-        }),
-        new CssMinimizerPlugin({}),
-      ],
-    },
-    output: {
-      path: path.join(__dirname, './frontend/web/dest'),
-      publicPath: '/dest/',
-      filename: `bundle.js?${assetsVersionId}`,
-    },
-    plugins: [
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/locale$/,
-        contextRegExp: /moment$/,
-      }),
-      new LodashModuleReplacementPlugin(),
-      new webpack.optimize.AggressiveMergingPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-        XDEBUG: false,
-        VERSION: null,
-        ...Object.keys(envVars).reduce((envs, envName) => {
-          // eslint-disable-next-line no-param-reassign
-          envs[envName] = JSON.stringify(envVars[envName])
-          return envs
-        }, {}),
-      }),
-      new MiniCssExtractPlugin({
-        filename: `[name].css?${assetsVersionId}`,
-        ignoreOrder: true,
-      }),
+      {
+        test: /\.scss$/, // For SASS/SCSS files
+        use: [
+          MiniCssExtractPlugin.loader, // Extract CSS into separate files
+          'css-loader', // Translate CSS into CommonJS
+          'postcss-loader', // Apply PostCSS transforms
+          'sass-loader', // Compile Sass into CSS
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|eot|ttf|otf)$/i, // Handle images/fonts
+        type: 'asset/resource',
+      },
     ],
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './frontend/web/src'),
-        fonts: path.resolve(__dirname, './frontend/web/fonts'),
-        img: path.resolve(__dirname, './frontend/web/img'),
-      },
-      extensions: ['.js'],
+  },
+
+  plugins: [
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'), // Ensure React uses production mode
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css', // Cache-busting for CSS files
+    }),
+    new CleanWebpackPlugin(), // Clean up the /dist folder
+  ],
+
+  optimization: {
+    minimize: true, // Enable minification
+    minimizer: [
+      new TerserWebpackPlugin({ // Minify JavaScript
+        terserOptions: {
+          compress: {
+            drop_console: true, // Strip console logs
+          },
+        },
+      }),
+      new CssMinimizerPlugin(), // Minify CSS
+    ],
+    splitChunks: {
+      chunks: 'all', // Split vendor code and app code
     },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          use: [{ loader: 'babel-loader' }],
-          include: [path.resolve(__dirname, 'frontend/web/src')],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            // 'style-loader',
-            'css-loader',
-          ],
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: ['autoprefixer'],
-                },
-              },
-            },
-            'sass-loader',
-          ],
-        },
-        {
-          test: /\.mjs$/,
-          include: /node_modules/,
-          type: 'javascript/auto',
-        },
-        {
-          test: /\.(woff2?|eot|otf|ttf|svg|png|jpe?g)/,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.anim\.json$/,
-          loader: 'file-loader',
-          type: 'javascript/auto',
-        },
-        {
-          test: /swagger\.json$/,
-          type: 'asset/resource',
-        },
-      ],
-    },
-  }
-}
+  },
+
+  performance: {
+    hints: false, // Disable performance hints for production
+  },
+};
